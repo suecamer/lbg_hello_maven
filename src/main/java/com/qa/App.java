abfdf0c02b67675f3fcd1a5a61596b69334c6edb
suecamer <suecamer@gmail.com>
2025-12-09 16:00:29 +0000

    EDIT: Version 3.0 snapshot changes

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,8 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Sue"));
+        System.out.println(sayHelloToSomeone("Harji"));
         System.out.println(sayGoodbye());
     }
 
